58e95c655f1fb9d459916bc383284558593e8801
dodohyuns <hdhcoss@gmail.com>
2024-05-09 21:36:18 +0900

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
 import { TypedRoute } from '@nestia/core';
 
 @Controller('/')
 export class AppController {
-  constructor(private appService: AppService) {}
-
   @TypedRoute.Get()
   get() {
     return [];
